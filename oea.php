82c7076ee730595bafbcfa803a9de93dabc5f951
jamieS95 <50465426+jamieS95@users.noreply.github.com>
2019-07-21 23:52:30 +0100

    Update oea.php

--- oea.php
+++ oea.php
@@ -1,19 +1,39 @@
 <?php
-if ($_GET['param1'] == 'information'){
-	$myObj->supportedversions = array('0.0.0.9','1.0.0.0');  
+if (isset() && ($_GET['param1'] == 'information')){
+	$myObj->supportedversions = array('0','1');  
 	$myJSON = json_encode($myObj); 
 	echo $myJSON;
 }else{
-	if($_GET['param2'] == '0.0.0.9'){
+	if($_GET['param2'] == '0'){
+		switch($_GET['param3']){
+			case 'coinlist':
+			
+				break;
+			case 'tradingpairs':
+			
+				break;
+			case 'account':
+			
+				break;
+			case 'neworder':
+			
+				break;
+			case 'dummyorder':
+			
+				break;
+			default:
+				$myObj->error = true;  
+				$myJSON = json_encode($myObj); 
+				echo $myJSON;				
+		}
 		
-		
+	}else{
+		$myObj->error = true;  
+		$myJSON = json_encode($myObj); 
+		echo $myJSON;		
 	}
 }
 
 
-
  
-
-
-
 ?>
